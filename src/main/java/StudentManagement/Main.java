a3ad7dd0eeb7049fe2d9c7f713b2a8a717c03949
mcenk <60326186+mcenk@users.noreply.github.com>
2022-07-02 16:27:33 +0300

    Ogrenci verileri guncellendi

--- src/main/java/StudentManagement/Main.java
+++ src/main/java/StudentManagement/Main.java
@@ -6,24 +6,25 @@ package StudentManagement;
                 Course fizik = new Course("Fizik", "FZK101", "FZK");
                 Course kimya = new Course("Kimya", "KMY101", "KMY");
 
-                Teacher t1 = new Teacher("Mahmut Hoca", "90550000000", "MAT");
-                Teacher t2 = new Teacher("Fatma Ayşe", "90550000001", "FZK");
-                Teacher t3 = new Teacher("Ali Veli", "90550000002", "KMY");
+                Teacher t1 = new Teacher("Arif Hoca", "00000002", "MAT");
+                Teacher t2 = new Teacher("Tarik Hoca", "00000003", "FZK");
+                Teacher t3 = new Teacher("Umran Hoca", "00000003", "KMY");
 
                 mat.addTeacher(t1);
                 fizik.addTeacher(t2);
                 kimya.addTeacher(t3);
+                fizik.printTeacher();
 
-                Student s1 = new Student("İnek Şaban", 4, "140144015", mat, fizik, kimya);
-                s1.addBulkExamNote(50,20,40);
+                Student s1 = new Student("Serra", 4, "12345", mat, fizik, kimya);
+                s1.addBulkExamNote(30,20,40);
                 s1.isPass();
 
-                Student s2 = new Student("Güdük Necmi", 4, "2211133", mat, fizik, kimya);
-                s2.addBulkExamNote(100,50,40);
+                Student s2 = new Student("Merve", 4, "123456", mat, fizik, kimya);
+                s2.addBulkExamNote(90,60,50);
                 s2.isPass();
 
-                Student s3 = new Student("Hayta İsmail", 4, "221121312", mat, fizik, kimya);
-                s3.addBulkExamNote(50,20,40);
+                Student s3 = new Student("Hakan", 4, "1234567", mat, fizik, kimya);
+                s3.addBulkExamNote(70,10,60);
                 s3.isPass();
 
             }
