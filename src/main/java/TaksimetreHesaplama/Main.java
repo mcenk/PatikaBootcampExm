772d3840d3373b79dfefd4f8dbe3a8fe46500939
mcenk <60326186+mcenk@users.noreply.github.com>
2022-07-04 23:17:59 +0300

    Taksimetre duzenlendi.Metod eklendi, static degerler uzerinde calisildi

--- src/main/java/TaksimetreHesaplama/Main.java
+++ src/main/java/TaksimetreHesaplama/Main.java
@@ -3,21 +3,20 @@ package TaksimetreHesaplama;
 import java.util.Scanner;
 
 public class Main {
-
+   static float acilis=10,toplamKm,toplamUcret=0;
         public static void main(String[] args) {
             float acilis=10,toplamKm,toplamUcret;
 
             Scanner scan=new Scanner(System.in);
             System.out.println("Toplam KM miktarini giriniz: ");
-            toplamKm=scan.nextFloat();
-
-            toplamUcret= (float) (acilis+(toplamKm*2.20));
+            System.out.println("Ödenecek ücret: "+ ucretHesapla(scan.nextFloat()));
+        }
+        static float ucretHesapla(float km){
+            toplamUcret= (float) (acilis+(km*2.20));
 
             if(toplamUcret<20)
                 toplamUcret=20;
 
-
-            System.out.println("Ödenecek ücret: "+toplamUcret);
-
+            return toplamUcret;
         }
 }
